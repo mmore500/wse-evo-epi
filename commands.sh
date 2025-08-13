0a7b7f31acf0f44cb7bd0570e96e3f5c5eec0815
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-08-13 17:50:45 -0400

    Add topic 7 sdk example code

---
+++ commands.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+$CSLC --arch=wse3 ./layout.csl --fabric-dims=10,5 --fabric-offsets=4,1 -o out \
+--memcpy --channels=1 --width-west-buf=0 --width-east-buf=0
+$CS_PYTHON run.py --name out
